1772d470385b632abe156f8a4e4a122de30ddaa9
Admin <admin@Admins-Mac-mini.home>
2018-07-02 01:00:38 -0400

    included last solution (commented)

--- index.js
+++ index.js
@@ -95,3 +95,18 @@ function rotateImage(matrix, n) {
     }
     return matrix;
 }
+
+// let permArr = [], usedChars = [];
+// function getPermutations(input) {
+//   for (let i = 0; i < input.length; i++) {
+//     let chunk = input.splice(i, 1)[0];
+//     usedChars.push(chunk);
+//     if (input.length == 0) permArr.push(usedChars.slice());
+//     getPermutations(input);
+//     input.splice(i, 0, chunk);
+//     usedChars.pop();
+//   }
+//   return permArr
+// };
+
+// console.log(JSON.stringify(getPermutations([1,2])));
